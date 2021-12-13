16c5b0a5d60a87796999c0166eb57f7f1e0d4781
Muhammad Adil <adilsaeed31@gmail.com>
2021-12-13 23:23:56 +0300

    Client side fetch functionality added

--- javascripts/pages/index.js
+++ javascripts/pages/index.js
@@ -2,4 +2,31 @@
 // Avoid using jquery if possible.
 $(document).ready(() => {
   console.log('Index page');
+
+  document.getElementById('ajax').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    document.querySelector('#ajaxSubmitBtn').disabled = true;
+    document.querySelector('#spinner').style.display = 'inline-block';
+
+    const data = {
+      token: document.querySelector('#token').value,
+      url: document.querySelector('#url').value,
+    };
+
+    fetch('http://localhost:3000/product', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.text())
+      .then((data) => {
+        document.querySelector('.index').innerHTML = data;
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  });
 });
