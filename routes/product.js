0688b45fe025e049891cf9bbda3a564005fbefb2
Muhammad Adil <adilsaeed31@gmail.com>
2021-12-13 21:56:21 +0300

    Product Page Design Finalize

--- routes/product.js
+++ routes/product.js
@@ -1,7 +1,8 @@
 const router = rootRequire('helpers/router.js');
 const { renderTemplate } = rootRequire('helpers/utils.js');
 
-router.post('/product',
+router.post(
+  '/product',
   (ctx, next) => {
     const { CrawlingAPI } = require('proxycrawl');
     const data = ctx.request.body;
@@ -9,19 +10,29 @@ router.post('/product',
 
     return api
       .get(data.url, { scraper: 'amazon-product-details' })
-      .then(response => {
+      .then((response) => {
         if (response.statusCode !== 200) {
-          throw new Error('Invalid ProxyCrawl status code: ' + response.statusCode + '\n' + response.body);
+          throw new Error(
+            'Invalid ProxyCrawl status code: ' +
+              response.statusCode +
+              '\n' +
+              response.body
+          );
         } else {
           ctx.product = JSON.parse(response.body).body;
-          console.log('Available product properties: ', ctx.product);
+          // console.log('Available product properties: ', ctx.product);
         }
         next();
       })
-      .catch(err => {
+      .catch((err) => {
         console.error(err);
         ctx.error = err;
         next();
       });
   },
-  (ctx) => ctx.body = renderTemplate('product', { error: ctx.error, product: ctx.product }));
+  (ctx) =>
+    (ctx.body = renderTemplate('product', {
+      error: ctx.error,
+      product: ctx.product,
+    }))
+);
